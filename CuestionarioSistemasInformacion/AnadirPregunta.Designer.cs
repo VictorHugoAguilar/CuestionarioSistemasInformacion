0f46c8a9d014828744162f6cf26aa9e79b81686b
Victor <victor03016@gmail.com>
2018-05-17 09:58:42 +0200

    ProgramaBase

---
+++ CuestionarioSistemasInformacion/AnadirPregunta.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace CuestionarioSistemasInformacion
+{
+    partial class AnadirPregunta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAnadiNuevaPregunta = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.lblAnadirPregunta = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAnadiNuevaPregunta
+            // 
+            this.btnAnadiNuevaPregunta.Location = new System.Drawing.Point(27, 437);
+            this.btnAnadiNuevaPregunta.Name = "btnAnadiNuevaPregunta";
+            this.btnAnadiNuevaPregunta.Size = new System.Drawing.Size(153, 61);
+            this.btnAnadiNuevaPregunta.TabIndex = 0;
+            this.btnAnadiNuevaPregunta.Text = "Añadir";
+            this.btnAnadiNuevaPregunta.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(218, 437);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(153, 61);
+            this.btnGuardar.TabIndex = 1;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(179, 56);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(791, 31);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(231, 343);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 31);
+            this.textBox5.TabIndex = 6;
+            // 
+            // lblAnadirPregunta
+            // 
+            this.lblAnadirPregunta.AutoSize = true;
+            this.lblAnadirPregunta.Location = new System.Drawing.Point(56, 62);
+            this.lblAnadirPregunta.Name = "lblAnadirPregunta";
+            this.lblAnadirPregunta.Size = new System.Drawing.Size(99, 25);
+            this.lblAnadirPregunta.TabIndex = 7;
+            this.lblAnadirPregunta.Text = "Pregunta";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 343);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(203, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Respuesta Correcta";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 25);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Respuesta 1";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(179, 135);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(791, 31);
+            this.textBox2.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(179, 189);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(791, 31);
+            this.textBox3.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 192);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 25);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Respuesta 1";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(179, 236);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(791, 31);
+            this.textBox4.TabIndex = 17;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 239);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 25);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Respuesta 1";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(179, 285);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(791, 31);
+            this.textBox6.TabIndex = 19;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 288);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 25);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Respuesta 1";
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.Location = new System.Drawing.Point(817, 437);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(153, 61);
+            this.btnVolver.TabIndex = 20;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = true;
+            this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
+            // 
+            // AnadirPregunta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1023, 548);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblAnadirPregunta);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnAnadiNuevaPregunta);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "AnadirPregunta";
+            this.Text = "AnadirPregunta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAnadiNuevaPregunta;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label lblAnadirPregunta;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnVolver;
+    }
+}
---
+++ CuestionarioSistemasInformacion/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CuestionarioSistemasInformacion
+{
+    public partial class Cuestionario : Form
+    {
+        public Cuestionario()
+        {
+            InitializeComponent();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+        //Creamos un DataSet
+        DataSet dsCuestionario;
+
+        //Creamos el DataAdapter
+        System.Data.OleDb.OleDbDataAdapter da;
+
+        //Creamos una variable que nos mostrara donde estamos situados
+        private int pos;
+
+        //Creamos una variable que mostrara el total de registros
+        private int totalRegistros;
+
+
+        private void Cuestionario_Load(object sender, EventArgs e)
+        {
+            //Creamos un objeto conexión y le damos la ruta de la bd
+            System.Data.OleDb.OleDbConnection con;
+            con = new System.Data.OleDb.OleDbConnection();
+
+            //Le pasamos la ruta de la conexión
+            con.ConnectionString = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:/Users/Victor/source/repos/CuestionarioSistemasInformacion/Recursos/Database1.accdb";
+
+            //Abrimos la conexión
+            con.Open();
+
+            //Creamos el objeto DataSet
+            dsCuestionario = new DataSet();
+
+            //Creamos el objeto utilizando una sentencia SQL para seleccionar los registros de la tabla
+            string sql = "Select * from Cuestionario";
+            da = new System.Data.OleDb.OleDbDataAdapter(sql, con);
+
+            da.Fill(dsCuestionario, "Cuestionario");
+
+            //Nos situamos en la primera posicion
+            pos = 0;
+            mostrarRegistro(pos);
+
+            //asignamos el total de registros
+            totalRegistros = dsCuestionario.Tables["Cuestionario"].Rows.Count;
+
+            //Cerramos la conexión
+            con.Close();
+        }
+
+        //Creamos un método que nos muestra donde estamos situados
+        private void mostrarRegistro(int pos)
+        {
+            //Objeto que nos permite recoger un registro de la tabla
+            DataRow dRegistro;
+
+            //Cogemos el registro en la posicion de la tabla cuestionario
+            dRegistro = dsCuestionario.Tables["Cuestionario"].Rows[pos];
+
+            //Cogemos el valor de cada una de las columnas del registro 
+            //y lo pasamos al labelbox correspondiente
+
+            lblPregunta.Text = dRegistro[1].ToString();
+            lblRespuesta1.Text = dRegistro[2].ToString();
+            lblRespuesta2.Text = dRegistro[3].ToString();
+            lblRespuesta3.Text = dRegistro[4].ToString();
+            lblRespuesta4.Text = dRegistro[5].ToString();
+        }
+
+        private void rbtn1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPregunta1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            if (pos < totalRegistros)
+                pos++;
+        }
+    }
+}
